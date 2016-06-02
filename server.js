f9742f02e43a6d36b259510836b09685e0725ee3
valeri <valericfbg@gmail.com>
2016-06-02 22:30:22 +0300

    user api is working

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 let express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    userRoutes = require('./api/routes/user/user.route');
 
-app.use(express.static('client'));  
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static('client'));  
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log('Listening on port 3000')
+app.use('/api/user', userRoutes);
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
