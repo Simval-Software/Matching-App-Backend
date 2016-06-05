1fba352d2f64d909dab23323cd47296155e4ab38
SimeonStoykov <simeon_stoykov@mail.bg>
2016-06-05 17:07:44 +0300

    improved user model

--- server/api/routes/user/user.model.js
+++ server/api/routes/user/user.model.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+let mongoose = require('mongoose'),
+Crypto = require('crypto'),
+Schema = mongoose.Schema;
 
-var userSchema = new Schema({
+let userSchema = new Schema({
     email: {
         type: String,
         required: true,
@@ -21,10 +22,18 @@ var userSchema = new Schema({
     zodiac: Number,
     country: String,
     city: String,
-    visitors: [ObjectId],
-    likedUsers: [ObjectId],
-    likedByUsers: [ObjectId],
-    matches: [ObjectId],
+    popularity: {
+        type: Number,
+        default: 0
+    },
+    visits: {
+        type: Number,
+        default: 0
+    },
+    visitors: [Schema.Types.ObjectId],
+    likedUsers: [Schema.Types.ObjectId],
+    likedByUsers: [Schema.Types.ObjectId],
+    matches: [Schema.Types.ObjectId],
     questionaries: [{
         id: Number,
         question: String,
@@ -36,8 +45,8 @@ var userSchema = new Schema({
     }],
     messages: [{
         id: Number,
-        sender: ObjectId,
-        reciever: ObjectId,
+        sender: Schema.Types.ObjectId,
+        reciever: Schema.Types.ObjectId,
         content: String
     }],
     images: [{
@@ -48,26 +57,24 @@ var userSchema = new Schema({
     profileImage: Number,
     salt: {
         type: String,
-        set: generateSalt
+        default: generateSalt
     },
     lastLoggedIn: Number
 });
 
-var User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema);
 
 module.exports = User;
 
-function generateSalt() {
-    //return Math.round((new Date().valueOf() * Math.random())) + '';
-
-    // Crypto.randomBytes('256', function(err, buf) {
-    //     if (err) throw err;
-    //     return buf;
-    // });
-
-    // return Crypto.randomBytes('256'); // fails to
+function toLower(text) {
+    return text.toLowerCase();
 }
 
-function encodePassword() {
+function generateSalt() {
+    return Crypto.randomBytes(256).toString("hex");
+}
 
+function encodePassword(password) {
+    password += 'pesho'
+    return password;
 }
